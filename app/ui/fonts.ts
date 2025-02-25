84b3ee31725f5712fd31cfffd0f986d78a8f13e6
murillobazz <murillobazilio@gmail.com>
2025-02-25 18:32:07 -0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Commissioner } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
+export const commissioner = Commissioner({ subsets: ['latin'], weight: ['400', '700'] });
